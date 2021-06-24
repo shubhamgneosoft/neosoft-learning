969e67cfe183023dc0f737c85f725fbbc511ab1f
Shubham Gupta <shubham.vimal@neosoftmail.com>
2021-06-24 20:07:02 +0530

    fixed the logout issue, added the pagination, and show the user's specific posts

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ import os
 import re
 
 from bson import json_util, ObjectId
-from flask import Flask, render_template, request, jsonify, session, json, url_for
+from flask import Flask, render_template, request, jsonify, session, json, url_for, session
 from flask_pymongo import PyMongo
 from dotenv import load_dotenv
 from flask_socketio import SocketIO, send, emit
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
+from flask_session import Session
 
 load_dotenv()
 
@@ -16,14 +17,19 @@ app.secret_key = 'e76gu687bcsk#dh@hkdk&vvb$7hvz'
 socketio = SocketIO(app)
 
 app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+app.config['SESSION_PERMANENT'] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 mongo = PyMongo(app)
 
 
 def is_login():
     is_user = False
-    if 'userid' in session and 'loggedin' in session:
+    userid = session.get("userid")
+    loggedin = session.get("loggedin")
+    if userid and loggedin:
         user_collection = mongo.db.users
-        query = {"_id": ObjectId(session['userid'])}
+        query = {"_id": ObjectId(session.get("userid"))}
         account = user_collection.count_documents(query)
         if account > 0:
             is_user = True
@@ -34,7 +40,7 @@ def is_login():
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if is_login:
+    if session.get('loggedin'):
         return redirect(url_for('blog'))
     msg = ''
     if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
@@ -110,20 +116,41 @@ def parse_json(data):
 
 
 @app.route('/blog')
-def blog():
-    if is_login:
-        post_collection = mongo.db.users_post
-        results = post_collection.find().sort("_id", -1)
-        return render_template('blog.html', post_data=results)
-    else:
+@app.route('/post/<mypost>')
+@app.route('/post/<mypost>/<page>')
+@app.route('/blog/<page>')
+def blog(mypost=None, page=1):
+
+    per_page_post = 5
+    end = int(page)*per_page_post
+    start = end - per_page_post
+    requested_page = '/blog'
+
+    if not session.get('loggedin'):
         return redirect(url_for('login'))
+
+    elif mypost == 'mypost':
+        post_collection = mongo.db.users_post
+        results = post_collection.find({'user': session.get("userid")}).sort("_id", -1)[start:end]
+    else:
+        post_collection = mongo.db.users_post
+        results = post_collection.find().sort("_id", -1)[start:end]
+    if mypost == 'mypost':
+        requested_page = '/post/mypost'
+
+    return render_template('blog.html', post_data=results, page=page, requested_page=requested_page)
 
 
 @socketio.on('message', namespace='/create_post')
 def create_post(content):
+    """
+
+    :param content: Text
+    :return: Html
+    """
     if content and is_login:
         post_collection = mongo.db.users_post
-        obj = post_collection.insert_one({'content': content, 'user': session["userid"]})
+        obj = post_collection.insert_one({'content': content, 'user': session.get("userid")})
         data = render_template('create_post.html', content=content, post_id=obj.inserted_id)
 
         send(data, broadcast=True)
@@ -144,7 +171,7 @@ def like_post(json_req):
         else:
             count = count + 1
             like_collection.insert_one({'post_id': post_id,
-                                        'user_id': session["userid"],
+                                        'user_id': session.get("userid"),
                                         'like_count': count
                                         })
 
@@ -159,7 +186,7 @@ def comment_on_post(json_req):
     if comment and post_id and is_login:
         comments_collection = mongo.db.users_comments
         comments_collection.insert_one({'comment': comment,
-                                        'user_id': session["userid"],
+                                        'user_id': session.get("userid"),
                                         'post_id': post_id})
         results = comments_collection.find({'post_id': post_id})
         comments = render_template('comments_on_post.html', results=results)
